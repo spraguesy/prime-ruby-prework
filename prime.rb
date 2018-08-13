96cdcd582ac9d1365fc20adbe2c7cb1cbfcfd07e
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 12:19:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  is_prime = true
+  if number == 1 || number == -1
+    return is_prime
+  end
+  
+  new_range = (2..(number-1)).to_a
+  puts new_range
+  new_range.each do |num|
+    if number % num == 0
+      is_prime = false
+    end
+  end
+  
+  return is_prime
+end
