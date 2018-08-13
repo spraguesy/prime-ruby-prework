3e867e41011d5a25638012f9f596050ccf7afcb3
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 12:26:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,15 @@
 # Add  code here!
 def prime?(number)
   is_prime = true
-  upper_range = number - 1
+  if number == 1 || number == || -1
+    return true
+  end
+  if number < 0
+    
+    upper_range = number * -1 - 1
+  else
+    upper_range = number - 1
+  end
   new_range = (2..upper_range).to_a
   
   new_range.each do |num|
